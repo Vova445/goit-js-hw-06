61d61991679cdf3199269d04b9624567ecca2027
Volodya Mikhnov <vvvmikhnov@gmail.com>
2023-09-28 19:34:02 +0300

    1

---
+++ src/js/task-02.js
@@ -0,0 +1,16 @@
+// Масив інгредієнтів
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
+];
+const ingredientsList = document.getElementById('ingredients');
+ingredients.forEach(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  ingredientsList.appendChild(listItem);
+});
