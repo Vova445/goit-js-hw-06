498c734f00cd2f690ada7656d2468e6c9c95defe
Volodya Mikhnov <vvvmikhnov@gmail.com>
2023-09-28 20:58:34 +0300

    task3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,58 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+const galleryList = document.querySelector(".gallery");
+
+const createGalleryItem = ({ url, alt }) => {
+  const listItem = document.createElement("li");
+  listItem.classList.add("gallery-items");
+
+  const image = document.createElement("img");
+  image.src = url;
+  image.alt = alt;
+  image.classList.add("gallery-images");
+
+  listItem.appendChild(image);
+  return listItem;
+};
+
+images.forEach((image) => {
+  const galleryItem = createGalleryItem(image);
+  galleryList.appendChild(galleryItem);
+});
+
+const cssElements = document.createElement("style");
+cssElements.innerHTML = `
+.gallery {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0;
+  margin: 0;
+  justify-content: center; 
+}
+
+.gallery-items {
+  flex: 1;
+  margin: 50px 20px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  overflow: hidden; 
+  transition: transform 0.2s; 
+  max-width: 450px;
+  border-radius: 30px; 
+}
+
+.gallery-items:hover {
+  transform: translateY(-5px) scale(1.1); 
+  box-shadow: 0 8px 12px rgba(0, 0, 0, 0.4); 
+}
+
+.gallery-images {
+  width: 100%; 
+  height: 100%;
+  border-radius: 8px; 
+}
+
+`;
+document.head.appendChild(cssElements);
