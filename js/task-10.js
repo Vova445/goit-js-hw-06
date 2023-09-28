ec913cac0e32f62db64a4e9347b3f07d7dafaf73
Volodya Mikhnov <vvvmikhnov@gmail.com>
2023-09-28 22:09:26 +0300

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
+const input = document.querySelector('input');
+const createBatton = document.querySelector('[data-create]');
+const destroyBatton = document.querySelector('[data-destroy]');
+const container = document.querySelector('#boxes');
+
+
+
+
+
+
+createBatton.addEventListener('click', create);
+destroyBatton.addEventListener('click', destroy);
+function create() {
+  const amount = input.value;
+  const boxes = [];
+  for (let i = 0; i < amount; i+=1) {
+    const box = document.createElement('div');
+    box.style.width = '30px';
+    box.style.height = '30px';
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+  container.append(...boxes);
+}
+function destroy() {
+  container.innerHTML = '';
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
