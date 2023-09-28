f95ac957928297cf398fdf0f9776f666ee0d4c99
Volodya Mikhnov <vvvmikhnov@gmail.com>
2023-09-28 21:57:52 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,32 @@
+const button = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+button.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const css = document.createElement("style");
+css.innerHTML = `
+.change-color {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+.change-color:hover {
+  background-color: blue;
+}
+
+`;
+
+document.head.appendChild(css);
